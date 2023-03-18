c289e391b68962c11d50e3c3221bea7f85d4fca9
Julio Chipa <juliochipa007@gmail.com>
2023-03-18 04:20:05 -0500

    El cambio de divisas ya lo realiza con normalidad, todo perfecto

--- src/ConversorDual/ConversorController.java
+++ src/ConversorDual/ConversorController.java
@@ -1,4 +1,4 @@
- package ConversorDual;
+package ConversorDual;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,6 +9,7 @@ import java.util.Collections;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextField;
@@ -36,11 +37,8 @@ public class ConversorController {
 
 	// Llamando a la clase API
 	API ApiDivisas = new API();
-	
-	Map<String, Double> tasaCambio;
-	
-	double PrecioDolar = ApiDivisas.ApiMap().get("USD");
 
+	Map<String, Double> tasaCambio;
 	DatosComboBox divisasLista = new DatosComboBox();
 
 	@FXML
@@ -49,10 +47,9 @@ public class ConversorController {
 		// System.out.println("Prueba para saber si entra a inialize");
 		// Cargando la API y lo Almacenamos en Map para que no este consultando cada
 		// digito ingresado
-		
+
 		tasaCambio = ApiDivisas.ApiMap();
-		
-		
+
 		Collections.sort(divisasLista.miLista);
 		ObservableList<String> items1 = FXCollections.observableArrayList(divisasLista.miLista);
 		cbxDivisa1.setItems(items1);
@@ -71,29 +68,45 @@ public class ConversorController {
 		txtDivisas1.setTextFormatter(new TextFormatter<>(num1Filtro));
 		txtDivisas2.setTextFormatter(new TextFormatter<>(num2Filtro));
 
+		// Esto es para cuadno Carga el Aplicativo
 		txtDivisas1.textProperty().addListener((observable, oldValue, newValue) -> {
-		
 			if (txtDivisas1.isFocused()) {
-		        CalculoDivisasUNO();
-		    }
-			
+				CalculoDivisasUNO();
+			}
 		});
 
 		txtDivisas2.textProperty().addListener((observable, oldValue, newValue) -> {
 			if (txtDivisas2.isFocused()) {
-		        CalculoDivisasDOS();
-		    }
+				CalculoDivisasDOS();
+			}
 		});
 
-		// Cuando Seleccionamos un ComboBox
+		// Cuando Seleccionamos un nuevo valor del comboBox1
+		cbxDivisa1.valueProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue != null) {
+				String NuevoValor = txtDivisas1.getText();
+				txtDivisas1.setText(NuevoValor);
+				CalculoDivisasUNO();
+			}
+		});
+
+		// Cuando Seleccionamos un nuevo valor del comboBox2
+		cbxDivisa2.valueProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue != null) {
+				String NuevoValor = txtDivisas2.getText();
+				txtDivisas2.setText(NuevoValor);
+				CalculoDivisasUNO();
+			}
+		});
 	}
 
 	private void CalculoDivisasUNO() {
-		String DatoCBX1 = cbxDivisa1.getValue(); // Capturando el valor actual del combobox
-		
+		String DatoCBX1 = cbxDivisa1.getValue(); // Capturando el valor actual del combobox1
+		String DatoCBX2 = cbxDivisa2.getValue(); // Capturando el valor actual del combobox2
+
 		Set<String> claves = tasaCambio.keySet(); // Capturamos las Claves del Map en un Set
-		//System.out.println(DatoCBX1);
-		
+		// System.out.println(DatoCBX1);
+
 		// Comparamos las claves que son tipo Set con el String DatoCBX1
 		if (claves.contains(DatoCBX1)) {
 			try {
@@ -101,25 +114,27 @@ public class ConversorController {
 				int numero1Entero = (int) numero1;
 				if (numero1 == numero1Entero) { // si el número es entero
 
-					double MontoConversion = numero1Entero * tasaCambio.get(DatoCBX1) / PrecioDolar ;
+					double MontoConversion = numero1Entero * tasaCambio.get(DatoCBX1) / tasaCambio.get(DatoCBX2);
 					txtDivisas2.setText(String.valueOf(MontoConversion)); // establecer el número como entero en el otro
-																	
+
 				} else {
-					double MontoConversion = numero1 * tasaCambio.get(DatoCBX1) / PrecioDolar ;
+					double MontoConversion = numero1 * tasaCambio.get(DatoCBX1) / tasaCambio.get(DatoCBX2);
 					txtDivisas2.setText(String.valueOf(MontoConversion)); // establecer el número como entero en el otro
 				}
 			} catch (NumberFormatException e) {
+				AlertaError();
 				e.printStackTrace();
 			}
 		}
 	}
 
 	private void CalculoDivisasDOS() {
-		String DatoCBX2 = cbxDivisa2.getValue(); // Capturando el valor actual del combobox
-			
+		String DatoCBX1 = cbxDivisa1.getValue(); // Capturando el valor actual del combobox1
+		String DatoCBX2 = cbxDivisa2.getValue(); // Capturando el valor actual del combobox2
+
 		Set<String> claves = tasaCambio.keySet(); // Capturamos las Claves del Map en un Set
-		//System.out.println(DatoCBX1);
-		
+		// System.out.println(DatoCBX1);
+
 		// Comparamos las claves que son tipo Set con el String DatoCBX1
 		if (claves.contains(DatoCBX2)) {
 			try {
@@ -127,17 +142,26 @@ public class ConversorController {
 				int numero2Entero = (int) numero2;
 				if (numero2 == numero2Entero) { // si el número es entero
 
-					double MontoConversion = numero2Entero * tasaCambio.get(DatoCBX2) / PrecioDolar ;
+					double MontoConversion = numero2Entero * tasaCambio.get(DatoCBX2) / tasaCambio.get(DatoCBX1);
 					txtDivisas1.setText(String.valueOf(MontoConversion)); // establecer el número como entero en el otro
-																	
+
 				} else {
-					double MontoConversion = numero2 * tasaCambio.get(DatoCBX2) / PrecioDolar ;
+					double MontoConversion = numero2 * tasaCambio.get(DatoCBX2) / tasaCambio.get(DatoCBX1);
 					txtDivisas1.setText(String.valueOf(MontoConversion)); // establecer el número como entero en el otro
 				}
 			} catch (NumberFormatException e) {
+				AlertaError();
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private void AlertaError() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Aviso de Error");
+		alert.setHeaderText(null);
+		alert.setContentText("Verifique si ingreso correctamente el numero");
+		alert.showAndWait();
 	}
 
 	// Creando el Filtro al ingresar datos al textField
